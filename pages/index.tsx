c7734a1e737e9eea8a909244f1e4dd4e224a03ce
BaekGeunYoung <dvmflstm@naver.com>
2019-12-11 15:06:29 +0900

    made home page & post detail page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,27 @@
-const Home = () => <h1>Hello world!</h1>;
+import React from 'react';
+import { NextPage } from 'next';
+
+import { getPosts, PostType } from '../api';
+import { MainLayout } from '../components/MainLayout';
+import { PostList } from '../components/PostList';
+
+interface Props {
+    posts: PostType[];
+}
+
+const Home: NextPage<Props> = ({ posts }) => (
+    <MainLayout title="홈">
+        <PostList posts={posts} />
+    </MainLayout>
+);
+
+Home.getInitialProps = async () => {
+    const { posts } = await getPosts();
+    console.log('executed home getInitialProps');
+
+    return {
+        posts
+    };
+};
 
 export default Home;
